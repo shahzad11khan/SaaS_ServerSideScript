f3fb6b8b63079b17569baa67f33fe86370040790
shahzad11khan <zarakali401@gmail.com>
2025-01-31 11:16:40 +0500

    login message

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,21 +8,21 @@ exports.login = async (req, res) => {
     try {
     const { email, password } = req.body;
     if (!email || !password) {
-        return res.status(400).json({ error: 'Email and password are required' });
+        return res.status(400).json({ message: 'Email and password are required' });
        }
     let user = await User.findOne({$and: [{ email: email }, { confirmPassword: password }]});
     let isCompany=null;
     if (!user) {
       isCompany = await Company.findOne({ email: email });
-      if(!isCompany) return res.status(400).json({ error: 'InCorrect Email' });
+      if(!isCompany) return res.status(400).json({ message: 'InCorrect Email' });
       user = isCompany;
     }
     const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid)  return res.status(400).json({ error: 'InCorrect password' });
-    if(user.status === 'inactive') return res.status(400).json({error:'User Is InActive'})
+    if (!isPasswordValid)  return res.status(400).json({ message: 'InCorrect password' });
+    if(user.status === 'inactive') return res.status(400).json({message:'User Is InActive'})
     const token = jwt.sign({ userId: user._id, userEmail:user.email,userName:user.username, permissions:user.permission , userImage:user.userLogoUrl, role: user.role }, process.env.JWT_SECRET, { expiresIn: '1y' });
     res.status(200).json({ message: 'Login successful', token });
   } catch (error) {
-    res.status(500).json({ error: 'Error logging in' });
+    res.status(500).json({ message: 'Error logging in' });
   }
 };
